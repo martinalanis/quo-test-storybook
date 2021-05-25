a3c2cea00197dcdcb27c90db71899f7f36e1d626
martinalanis <martinalanis.dev@gmail.com>
2021-05-25 13:09:31 -0500

    feat: Button component with initial props

---
+++ components/ui/Button.vue
@@ -0,0 +1,37 @@
+<template>
+  <button
+    :disabled="!active"
+    @click="$emit('clicked', icon)"
+  >
+    {{ localIcon }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    icon: {
+      type: String,
+      default: 'plus'
+    },
+    active: {
+      type: Boolean,
+      default: true
+    }
+  },
+  computed: {
+    localIcon () {
+      const availableIcons = {
+        plus: '+',
+        minus: '-'
+      }
+      return availableIcons[this.icon] || availableIcons.plus
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
